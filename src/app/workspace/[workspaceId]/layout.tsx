20698c3afa79d67ef268fb506106e1e4fb8d8870
nlisnyak <lisnyaknikita@gmail.com>
2024-10-06 21:26:35 +0300

    feat: started implementing channels sidebar

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable'
 import React from 'react'
 import { Sidebar } from './sidebar'
 import { Toolbar } from './toolbar'
+import { WorkspaceSidebar } from './workspace-sidebar'
 
 interface IWorkspaceIdLayoutProps {
 	children: React.ReactNode
@@ -14,7 +16,13 @@ const WorkspaceIdLayout = ({ children }: IWorkspaceIdLayoutProps) => {
 			<Toolbar />
 			<div className='flex h-[calc(100vh-40px)]'>
 				<Sidebar />
-				{children}
+				<ResizablePanelGroup direction='horizontal' autoSaveId={'ca-workspace-layout'}>
+					<ResizablePanel defaultSize={20} minSize={11} className='bg-[#5e2c5f]'>
+						<WorkspaceSidebar />
+					</ResizablePanel>
+					<ResizableHandle withHandle />
+					<ResizablePanel minSize={20}>{children}</ResizablePanel>
+				</ResizablePanelGroup>
 			</div>
 		</div>
 	)
