63f83694d906bf93a95509703b3c98f2792e078a
nlisnyak <lisnyaknikita@gmail.com>
2024-10-06 14:33:40 +0300

    feat: started implementing sidebar and members functionality

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React from 'react'
+import { Sidebar } from './sidebar'
 import { Toolbar } from './toolbar'
 
 interface IWorkspaceIdLayoutProps {
@@ -11,7 +12,10 @@ const WorkspaceIdLayout = ({ children }: IWorkspaceIdLayoutProps) => {
 	return (
 		<div className='h-ful'>
 			<Toolbar />
-			{children}
+			<div className='flex h-[calc(100vh-40px)]'>
+				<Sidebar />
+				{children}
+			</div>
 		</div>
 	)
 }
